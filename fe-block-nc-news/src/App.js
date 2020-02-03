4bfa602073552a2ce468b8991776938771138840
ItchyNg <andyng06@hotmail.com>
2020-02-03 14:02:48 +0000

    basic set up

--- fe-block-nc-news/src/App.js
+++ fe-block-nc-news/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import DisplayArticles from "./components/DisplayArticles";
+import { Router } from "@reach/router";
+import Homepage from "./components/Homepage";
+import TopicsPage from "./components/TopicsPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navbar />
+      <Router>
+        <Homepage path="/" />
+        <TopicsPage path="/topics" />
+      </Router>
     </div>
   );
 }

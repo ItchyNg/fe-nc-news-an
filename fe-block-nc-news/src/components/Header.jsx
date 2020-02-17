03006e809b273e86da479654ba3bcdc09d22e8a7
ItchyNg <andyng06@hotmail.com>
2020-02-17 15:57:48 +0000

    removed nested folders

--- fe-block-nc-news/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = () => {
-  return (
-    <img src="Untitled.png" alt="The NorthCoders News Logo" width="400px"></img>
-  );
-};
-
-export default Header;
